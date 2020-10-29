2ad684f6d660750fb3d1a06b6f2ae5e4aca33673
pparkst <pparkst@naver.com>
2020-10-30 01:27:06 +0900

    .

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+insertYear = input("인구수를 알고 싶은 연도를 입력하세요.");
 
 df = pd.read_csv('population.csv')
 df.head()
@@ -10,14 +13,14 @@ x = df["year"]
 y = df["count"]
 
 plt.plot(x, y, 'o') #x, y insert 'o' 
-plt.show() # show
+# plt.show() # show
 
 line_fitter = LinearRegression()
 line_fitter.fit(x.values.reshape(-1,1), y)
 # values reshape를 한 이유는 다중회귀분석 때문에 배열로 넣어야해서 넣음
 
-res = line_fitter.predict([[2021]])
-print(res)
+res = line_fitter.predict([[int(insertYear)]])
+print(str(math.ceil(res)) + " 명")
 res2 = line_fitter.coef_  #기울기
 res3 = line_fitter.intercept_  #질펀
 #print(res2)
@@ -27,3 +30,5 @@ res3 = line_fitter.intercept_  #질펀
 plt.plot(x, y, 'o')
 plt.plot(x, line_fitter.predict(x.values.reshape(-1,1)))
 plt.show()
+
+exit()
